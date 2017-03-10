7a0af4591aef9f5a6d1178d89e7f4f51d5875b96
roydavid957 <r.a.david@student.rug.nl>
2017-03-10 17:18:26 +0100

    final version, added .txt
    final version of sh script, added .txt file used for the script

--- sample-tweets.sh
+++ sample-tweets.sh
@@ -6,8 +6,14 @@
 # 	there are in a sample of tweets and shows the first 20 unique tweets
 # 	that are not retweets
 #
-# Usage:./sample-tweets.sh FILE(/net/corpora/twitter2/Tweets/2016/08/2017040212.out.gz | /net/corpora/twitter2/tools/tweet2tab user text)
+# Usage:./sample-tweets.sh FILE(20170301:12.txt)
+#	download the .txt file from: https://github.com/roydavid957/iwo-project
+#	to create .txt file: zcat /net/corpora/twitter2/Tweets/2017/03/20170301\:12.out.gz | /net/corpora/twitter2/tools/tweet2tab text > FILENAME
 
+echo -e "\e[33mUsage:./sample-tweets.sh FILE(20170301:12.txt)"
+echo -e "	download the .txt file from: https://github.com/roydavid957/iwo-project"
+echo -e "	to create .txt file: zcat /net/corpora/twitter2/Tweets/2017/03/20170301\:12.out.gz | /net/corpora/twitter2/tools/tweet2tab text > FILENAME\e[0m"
+echo ""
 # argument is the file, check if we get it
 TEXT=$1
 if [ -z "$TEXT" ]
@@ -20,7 +26,7 @@ cat $TEXT | wc -l
 # counts unique tweets
 cat $TEXT | uniq | wc -l
 # counts RT's within unique tweets
-cat $TEXT | uniq | grep -c 'RT'
-# show the first 20 unique tweets in the sample that are not retweets
-cat $TEXT | uniq | grep -v 'RT' | head -20
+cat $TEXT | uniq | grep -c -w 'RT'
+#show the first 20 unique tweets in the sample that are not retweets
+cat $TEXT | uniq | grep -v -w 'RT' | head -20
 
