6ea4e47813f29782bc174e872e6b03405a75a51d
roydavid957 <r.a.david@student.rug.nl>
2017-03-20 21:39:42 +0100

    second updated version
    used extra list instead of file, made the code more clear

--- iwo_fp.py
+++ iwo_fp.py
@@ -7,28 +7,33 @@ import os
 #from pandas_datareader import data, wb
 
 def main(): #def main(argv):
+	#makedata
 	#os.system("zcat /net/corpora/twitter2/Tweets/2016/08/20160815*.out.gz | /net/corpora/twitter2/tools/tweet2tab user text > 20160815.txt")
+
 	f = open("20160815.txt", "r")
-	emojis = [":D",";D",":-D",";-D",":)",";)",":-)",";-)","<3",":P",";P",":-P",";-P","XD","xD","x-D",":'D",
-		":(",":-(","</3",":')",":'(","*-*","-_-","0_0",":o",":O",":-o",":-O",":'O",":'o",">.<",">,<",":*",";*"]
-	elist = []
-	emojicntr = 0
+
+	#emojilist
+	emojis = ["^-^","o.O","O.o",">:O",":v",":3",":D",";D",":-D",";-D",":)",";)",":-)",";-)","<3",":P",";P",":-P",";-P","XD","xD","x-D",":'D",":(",":-(","</3",":')",":'(","*-*","-_-","0_0",":o",":O",":-o",":-O",":'O",":'o",">.<",">,<",":*",";*",":|",":/",":]",";]","B|","B-)",">:("]
+	#emojis = [":)",":(",":P",":D",":O",";)","B-)","B|",">:(",":/",":'(","3:)","O:)",":*","<3","^-^","-_-","o.O","O.o",">:O",":v",":3"] #http://emojicodes.com/
+
+	#ageranges
 	young = [str(i) for i in range(1986,2011)]
 	old = [str(i) for i in range(1930,1976)]
 	agerange = [str(i) for i in range(1930,2011)]
-	oldcntr = 0
-	youngcntr = 0
-	fage = open("twwage.txt", "w")
+
+	#tweets_w_age_list
+	tweets_w_age = []
 	for line in f:
 		line = line.split("\t")
 		username = line[0]
 		if any(age in username[-4:] for age in agerange):
-			fage.write("\t".join(line))
-	fage.close()
-	fage = open("twwage.txt", "r")
-	for line in fage:
+			tweets_w_age.append(line)
+
+	#emojicntr&list
+	elist = []
+	emojicntr = 0
+	for line in tweets_w_age:
 		emoji = ""
-		line = line.split("\t")
 		for c in line[1].split():
 			if c in emojis:
 				emoji = True
@@ -37,6 +42,10 @@ def main(): #def main(argv):
 			emojicntr += 1
 	print("ec",emojicntr)
 	print("el",len(elist))
+
+	#results
+	oldcntr = 0
+	youngcntr = 0
 	for line in elist:
 		username = line[0]
 		if any(age in username for age in agerange):
@@ -47,7 +56,7 @@ def main(): #def main(argv):
 	print("o",oldcntr)
 	print("y",youngcntr)
 	print("t",youngcntr+oldcntr)
+
 	f.close()
-	fage.close()
 if __name__ == "__main__":
 	main() #main(sys.argv)
